47ef8efcc575bff078868c3019cbace8b7876b70
Henri <henri.breuer@outlook.de>
2024-02-12 17:00:29 +0100

    improve title animation

--- portfolio_v2/src/components/AnimatedLetters/index.jsx
+++ portfolio_v2/src/components/AnimatedLetters/index.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react';
 import './index.scss'
 
 const AnimatedLetters = ({ letterClass, strArray, idx }) => {
+  const [hoveredIndices, setHoveredIndices] = useState([]);
+
+  const handleMouseEnter = (i) => {
+    setHoveredIndices((prevIndices) => [...prevIndices, i]);
+  };
+
+  const handleAnimationEnd = (i) => {
+    setHoveredIndices((prevIndices) => prevIndices.filter(index => index !== i));
+  };
+
   return (
     <span className='test'>
       {strArray.map((char, i) => (
-        <span key={char + i} className={`${letterClass} _${i + idx}`}>
+        <span 
+          key={char + i} 
+          className={`${letterClass} _${i + idx} ${hoveredIndices.includes(i) ? 'bounceIn' : ''}`}
+          onMouseEnter={() => handleMouseEnter(i)}
+          onAnimationEnd={() => handleAnimationEnd(i)}
+        >
           <h2>{char}</h2>
         </span>
       ))}
